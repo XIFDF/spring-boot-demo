df91ac4fdd16809be25c189356176dec74807044
XIFDF <2398869479@qq.com>
2018-12-12 17:28:37 +0800

    弃用jsp，使用thymeleaf

--- src/main/java/com/xifdf/springbootdemo/mapper/UserMapper.java
+++ src/main/java/com/xifdf/springbootdemo/mapper/UserMapper.java
@@ -2,8 +2,10 @@ package com.xifdf.springbootdemo.mapper;
 
 import com.xifdf.springbootdemo.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component("userMapper")
 public interface UserMapper {
 
     User getUserById(String userid);
